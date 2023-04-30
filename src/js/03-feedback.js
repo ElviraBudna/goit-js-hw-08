f85e52da07b23281ef093683ddafcbe9d667dc87
ElviraBudna <elvira.budna@gmail.com>
2023-04-30 20:05:16 +0300

    m

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,35 +1,4 @@
 import throttle from 'lodash.throttle';
-// const formEl = document.querySelector('.feedback-form');
-// const STORAGE_KEY = 'feedback-form-state';
-// const formData = { email: '', message: '' };
-// const inputEl = document.querySelector('input');
-// const textareaEl = document.querySelector('textarea');
-
-// formEl.addEventListener('submit', onSubmitForm);
-// formEl.addEventListener('input', throttle(onInput, 500));
-// populateInputForm();
-
-// function onSubmitForm(evt) {
-//   evt.preventDefault();
-//   console.log(populateInputForm());
-//   formEl.reset();
-//   localStorage.removeItem(STORAGE_KEY);
-// }
-
-// function onInput(evt) {
-//   formData.email === null ? '' : inputEl.value.trim();
-//   formData.message === null ? '' : textareaEl.value.trim();
-//   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-// }
-
-// function populateInputForm(evt) {
-//   const parsedFormData = JSON.parse(localStorage.getItem(STORAGE_KEY));
-//   parsedFormData.email === null ? '' : (inputEl.value = parsedFormData.email);
-//   parsedFormData.message === null
-//     ? ''
-//     : (textareaEl.value = parsedFormData.message);
-//   return parsedFormData;
-// }
 
 const STORAGE_KEY = 'feedback-form-state';
 const formData = {};
